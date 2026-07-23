5953b4f96722a63364e4442a8d6fda610dabaf6f
joaozinhoDoGrauXd <joaoninguem1301@gmail.com>
2026-07-23 13:07:06 -0300

    fix: return to SDK 54

--- src/components/custom/auth/Copyright.tsx
+++ src/components/custom/auth/Copyright.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Text } from "@/components/ui/text";
+import  Constants  from "expo-constants";
 
 export default function Copyright() {
+  const version =  Constants?.expoVersion
   return (
     <Text className="text-zinc-500 text-[11px] mt-7 z-10 font-semibold tracking-wide">
-      YourSpot App • Versão 1.0.0
+      YourSpot App • Versão {version}
     </Text>
   );
 }
